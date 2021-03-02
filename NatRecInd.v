f381ee5413dfa744e4c5b20e0aa26f07e006a5f1
João Guilherme <joguicosta@hotmail.com>
2021-03-02 16:15:46 -0300

    Testando

--- NatRecInd.v
+++ NatRecInd.v
@@ -1,8 +1,3 @@
-(* Definindo os números naturais *)
-Inductive nat : Type :=
-  | O
-  | S (n : nat).
-
 (* Definindo a soma *)
 Fixpoint plus (n : nat) (m : nat) : nat :=
   match m with
@@ -10,29 +5,45 @@ Fixpoint plus (n : nat) (m : nat) : nat :=
     | S m' => S (plus n m')
   end.
 
-(* Definindo a multiplicação *)
+(* Exercício x4.5 [Sem recursão] *)
+Definition func_d1 (n : nat) : nat :=
+  plus n n.
+
+(* Exercício x4.5 [Com recursão] *)
+Fixpoint func_d2 (n : nat) : nat :=
+  match n with
+    | O => O
+    | S n' => S (S (func_d2 n'))
+  end.
+
+(* Definindo a multiplicação [Exercício x4.6] *)
 Fixpoint multi (n : nat) (m : nat) : nat :=
   match m with
     | O => O
     | S m' => plus n (multi n m')
   end.
 
-(* Definindo a exponenciação *)
+(* Definindo a exponenciação [Exercício x4.9] *)
 Fixpoint expo (n : nat) (m : nat) : nat :=
   match m with
     | O => S m
     | S m' => multi n (expo n m')
   end.
 
+(* Definindo a sequência de Fibonacci [Exercício x4.11] *)
+Fixpoint fibo (n : nat) : nat :=
+  match n with
+  | O => O
+  | S O => S O
+  | S (S n' as n'') => fibo n'' + fibo n' 
+  end.
+
 Notation "x + y" := (plus x y)
-                       (at level 50, left associativity)
-                       : nat_scope.
+                       (at level 50, left associativity).
 Notation "x * y" := (multi x y)
-                       (at level 40, left associativity)
-                       : nat_scope.
+                       (at level 40, left associativity).
 Notation "x ^ y" := (expo x y)
-                       (at level 30, right associativity)
-                       : nat_scope.
+                       (at level 30, right associativity).
 
 (* Comutatividade da soma *)
 Theorem plus_comm : forall n m : nat,
@@ -57,4 +68,4 @@ Proof.
   induction p as [| p' IHp'].
   - simpl. reflexivity.
   - simpl. rewrite <- IHp'. reflexivity.
-Qed. 
+Qed.
