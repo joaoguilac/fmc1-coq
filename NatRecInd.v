fb9287593edd1b8f2eec0b2d812d4a389a20f9cb
João Guilherme <joguicosta@hotmail.com>
2021-04-09 02:22:26 -0300

    Adicionei propriedades da multiplicação

--- NatRecInd.v
+++ NatRecInd.v
@@ -39,26 +39,40 @@ Fixpoint fibo (n : nat) : nat :=
   end.
 
 Notation "x + y" := (plus x y)
-                       (at level 50, left associativity).
+                       (at level 50, left associativity)
+                       : nat_scope.
 Notation "x * y" := (multi x y)
-                       (at level 40, left associativity).
+                       (at level 40, left associativity)
+                       : nat_scope.
 Notation "x ^ y" := (expo x y)
-                       (at level 30, right associativity).
+                       (at level 30, right associativity)
+                       : nat_scope.
 
 (* Comutatividade da soma *)
 Theorem plus_comm : forall n m : nat,
   n + m = m + n.
 Proof.
   induction n as [| n' IHn'].
-  - (* n = 0 *)
+  - (* n = 0 (BASE) *)
     induction m as [| m' IHm'].
-    + simpl. reflexivity.
-    + simpl. rewrite -> IHm'. simpl. reflexivity.
-  - (* n = S n' *)
+    + simpl. 
+      reflexivity.
+    + simpl.
+      rewrite -> IHm'.
+      simpl.
+      reflexivity.
+  - (* n = S n' (P.I.) *)
     induction m as [| m' IHm'].
-    + simpl. rewrite <- IHn'. simpl. reflexivity.
-    + simpl. rewrite <- IHn'. rewrite -> IHm'. 
-      simpl. rewrite -> IHn'. reflexivity.
+    + simpl.
+      rewrite <- IHn'.
+      simpl.
+      reflexivity.
+    + simpl.
+      rewrite <- IHn'.
+      rewrite -> IHm'. 
+      simpl.
+      rewrite -> IHn'.
+      reflexivity.
 Qed.
 
 (* Associatividade da soma *)
@@ -68,4 +82,79 @@ Proof.
   induction p as [| p' IHp'].
   - simpl. reflexivity.
   - simpl. rewrite <- IHp'. reflexivity.
+Qed.
+
+Lemma lemma_succ : forall (n : nat), S n = n + (S O).
+Proof.
+  intros n.
+  simpl.
+  reflexivity.
+Qed.
+
+(* Distributividade da multiplicação *)
+Theorem multi_distri : forall n m p : nat,
+  n * (m + p) = (n * m) + (n * p).
+Proof.
+  induction p as [| p' IHp'].
+  - simpl. 
+    reflexivity.
+  - simpl.
+    rewrite -> IHp'.
+    rewrite -> (plus_assoc n (n*m) (n*p')).
+    rewrite -> (plus_assoc (n*m) (n) (n*p')).
+    rewrite -> (plus_comm (n*m) (n)).
+    reflexivity.
+Qed.
+
+(* Comutatividade da multiplicação *)
+Theorem multi_comm : forall n m : nat,
+  n * m = m * n.
+Proof.
+  induction n as [| n' IHn'].
+  - (* BASE *)
+    induction m as [| m' IHm'].
+    + (* SUB-BASE-1 *) 
+      simpl.
+      reflexivity.
+    + (* SUB-P.I.-1 *) 
+      simpl.
+      rewrite -> IHm'.
+      simpl.
+      reflexivity.
+  - (* P.I. *)
+    induction m as [| m' IHm'].
+    + (* SUB-BASE-2 *) 
+      simpl.
+      rewrite <- IHn'.
+      simpl.
+      reflexivity.
+    + (* SUB-P.I.-2 *) 
+      simpl.
+      rewrite -> IHm'.
+      rewrite <- IHn'.
+      simpl.
+      rewrite <- IHn'.
+      rewrite -> (plus_assoc (S n') m' (n'*m')).
+      rewrite -> (plus_assoc (S m') n' (n'*m')).
+      rewrite -> (lemma_succ (n')).
+      rewrite -> (lemma_succ (m')).
+      rewrite -> (plus_comm n' (S O)).
+      rewrite <- (plus_comm (S O) m').
+      rewrite <- (plus_assoc (S O) n' m').
+      rewrite -> (plus_comm n' m').
+      rewrite -> (plus_assoc (S O) m' n').
+      reflexivity.
+Qed.
+
+(* Associatividade da multiplicação *)
+Theorem multi_assoc : forall n m p : nat,
+  n * (m * p) = (n * m) * p.
+Proof.
+  induction p as [| p' IHp'].
+  - simpl.
+    reflexivity.
+  - simpl.
+    rewrite -> (multi_distri n m (m*p')).
+    rewrite -> IHp'.
+    reflexivity.
 Qed.
